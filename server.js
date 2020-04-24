672a88f0bdce9784bcc347db79d2f9f6b523c5b1
ananya92 <1992ananya@gmail.com>
2020-04-24 12:24:23 +0930

    Importing passport code and connecting to mongoose database

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+var session = require("express-session");
+var bodyParser = require('body-parser');
+
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Express Session - We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard user", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(routes);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/whatsplanDb");
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
