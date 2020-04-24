03bd084786169b86a360694d0c33f93ec622c062
ananya92 <1992ananya@gmail.com>
2020-04-24 18:09:41 +0930

    Installed Semantic react and spectre.css

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from 'react-router-dom'
+import 'spectre.css/dist/spectre.min.css';
+import 'spectre.css/dist/spectre-icons.css';
 
 ReactDOM.render(
     <BrowserRouter>
