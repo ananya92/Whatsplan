179f4eb92d14dd67cda4833174a19e0420edabe3
ananya92 <1992ananya@gmail.com>
2020-04-26 09:54:19 +0930

    Serializing and storing the entire user object in the session instead of only the username

--- config/passport/index.js
+++ config/passport/index.js
@@ -2,30 +2,19 @@ const passport = require('passport');
 const LocalStrategy = require('./localStrategy');
 const User = require('../../models/user');
 
-// called on login, saves the id to session req.session.passport.user = {id:'..'}
 passport.serializeUser((user, done) => {
 	console.log('*** serializeUser called, user: ')
 	console.log(user) // the whole raw user object!
 	console.log('---------')
-	done(null, { _id: user._id })
+	done(null, user)
 })
 
-// user object attaches to the request as req.user
-passport.deserializeUser((id, done) => {
-	console.log('DeserializeUser called')
-	User.findOne(
-		{ _id: id },
-		'username',
-		(err, user) => {
-			console.log('*** Deserialize user, user:')
-			console.log(user)
-			console.log('--------------')
-			done(null, user)
-		}
-	)
+passport.deserializeUser((obj, done) => {
+	console.log('DeserializeUser called');
+	done(null, obj);
 })
 
 //  Use Strategies 
-passport.use(LocalStrategy)
+passport.use(LocalStrategy);
 
-module.exports = passport
+module.exports = passport;
--- config/passport/localStrategy.js
+++ config/passport/localStrategy.js
@@ -7,25 +7,19 @@ const strategy = new LocalStrategy(
 	},
 	function(username, password, done) {
 		console.log("Reached here local strategy", username, password);
-		User.find({},(err, users) => {
-			console.log("Reached here findAll error", err, users);
-		});
 		User.findOne({ username: username }, (err, user) => {
-			console.log("Reached here err", err);
 			if (err) {
 				return done(err)
 			}
-			console.log("Reached here user", user);
 			if (!user) {
 				return done(null, false, {status: "error", message: 'Incorrect username' })
 			}
-			console.log("Reached here local strategy", user.checkPassword(password));
 			if (!user.checkPassword(password)) {
 				return done(null, false, {status: "error", message: 'Incorrect password' })
 			}
-			return done(null, user)
+			return done(null, user);
 		})
 	}
 )
 
-module.exports = strategy
+module.exports = strategy;
