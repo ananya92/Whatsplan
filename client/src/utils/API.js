9a7b43ee1e8b9258455939f05d09fffa87d6b8f8
ananya92 <1992ananya@gmail.com>
2020-04-26 09:59:03 +0930

    Added API to create new plan

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -21,5 +21,16 @@ export default {
     // get all registered users
     getRegisteredUsers: function() {
         return axios.get("/api/user/all");
+    },
+    // get user by email
+    getUserByEmail: function(email) {
+        return axios.get(`/api/user/getUserByEmail/${email}`);
+    },
+    // post new plan
+    newPlan: function(postData) {
+        return axios.post("/api/wp/plan", postData);
+    },
+    addPlanToUser: function(email, plan_id) {
+        return axios.put(`/api/user/addPlanToUser/${email}`, {plan_id: plan_id});
     }
 };
--- routes/api/wpRoutes.js
+++ routes/api/wpRoutes.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
 const controller = require("../../controllers/controller");
+const Plan = require("../../models/plan");
 
 // Matches with "/api/wp"
-
+router.post('/plan', (req, res) => {
+    console.log('post new plan', req.body);
+    const newPlan = {
+        title: req.body.title,
+        owner: req.body.owner,
+        members: req.body.members,
+        status: req.body.status
+    }
+    Plan.create(newPlan, function (err, savedPlan) {
+        if (err) return res.json(err);
+        res.json(savedPlan);
+    });
+});
 module.exports = router;
 
