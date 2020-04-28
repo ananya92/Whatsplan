2a9990586df6a3edf165dba63cbeb63b7e5391d1
ananya92 <1992ananya@gmail.com>
2020-04-28 13:43:30 +0930

    Created new task component

---
+++ client/src/components/task.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import API from "../utils/API";
+
+function Task(props) {
+    const [taskState, setTaskState] = useState({
+        task: null
+    });
+    useEffect(() => {
+
+    }, []);
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Task;
