65c71f9094fa01cff6624a2732c223f7e45d3bdf
ananya92 <1992ananya@gmail.com>
2020-04-30 01:16:15 +0930

    Installed moment.js to calculate the number of days remaining days for the task and used Semantic progress bar to display the task progress status

--- client/src/components/task.js
+++ client/src/components/task.js
@@ -3,9 +3,11 @@ import API from "../utils/API";
 import { Table } from 'semantic-ui-react';
 import { usePlanContext } from "../utils/GlobalState";
 import history from "../utils/history";
+import { Progress } from 'semantic-ui-react';
+import moment from 'moment';
 
 function Task(props) {
-    const [_, dispatch] = usePlanContext();
+    const [state, dispatch] = usePlanContext();
     const [taskState, setTaskState] = useState({
         task: null
     });
@@ -15,23 +17,49 @@ function Task(props) {
     const [taskAsigneeState, setTaskAsigneeState] = useState({
         asigneeName: ""
     })
+    const [progressState, setProgressState] = useState({
+        percentage: 0,
+        overdue: false,
+        text: ""
+    });
     useEffect(() => {
         API.getTask(props.taskId).then(response => {
             console.log("Got task:", response);
             setTaskState({ task: response.data });
+
+            // Determining the progress bar
+            if (response.data.startDate) {
+                var start = moment(response.data.startDate);
+                var now = moment();
+                var end = moment(response.data.endDate);
+                var totalDays = (end.diff(start, 'days'));
+                var passedDays = (now.diff(start, 'days'));
+                if (end.diff(now, 'days') < 0) {
+                    setProgressState({ ...progressState, percentage: 100, overdue: true, text: "Overdue" });
+                }
+                else if (response.data.status === "Done") {
+                    setProgressState({ ...progressState, percentage: 100 });
+                }
+                else {
+                    var percentageProgress = Math.floor((passedDays / totalDays) * 100);
+                    var remainingDays = totalDays - passedDays;
+                    setProgressState({ ...progressState, percentage: percentageProgress, text: `${remainingDays} days left` });
+                }
+            }
+
             // determine color based on task state
-            switch(response.data.status) {
+            switch (response.data.status) {
                 case "Pending":
-                    setColorState({ color: "grey"})
+                    setColorState({ color: "grey" })
                     break;
                 case "On It":
-                    setColorState({ color: "blue"})
+                    setColorState({ color: "purple" })
                     break;
                 case "Done":
-                    setColorState({ color: "green"})
+                    setColorState({ color: "green" })
                     break;
                 case "Stuck":
-                    setColorState({ color: "red"})
+                    setColorState({ color: "red" })
                     break;
             }
             API.getUserById(response.data.asignee).then(response1 => {
@@ -43,34 +71,48 @@ function Task(props) {
         }).catch(error => {
             console.log("Error while getting task by id: ", error);
         });
+
     }, []);
     function handleTaskClick(event) {
         event.preventDefault();
         // saving the task as the current task  and the milestone as current milestone in Global Store
-        API.getMilestoneByTaskId(taskState.task._id).then(function(response) {
-            dispatch({ type: "initMilestone", data:  response.data});
+        API.getMilestoneByTaskId(taskState.task._id).then(function (response) {
+            dispatch({ type: "initMilestone", data: response.data });
             dispatch({ type: "initTask", data: taskState.task });
             history.push("/taskInfo");
         })
-        .catch(error => {
-            console.log("Error while getting task by id: ", error);
-        });
+            .catch(error => {
+                console.log("Error while getting task by id: ", error);
+            });
     }
     return (
         <div>
             {taskState.task ?
-            <Table fixed singleLine unstackable>
-            <Table.Body>
-                <Table.Row>
-                    <Table.Cell width={5}>
-                        <a href="" onClick = {(event) => {handleTaskClick(event)}}>{taskState.task.taskName}</a>                    
-                    </Table.Cell>
-                    <Table.Cell width={5}>{taskAsigneeState.asigneeName}</Table.Cell>
-                    <Table.Cell width={3} className={colorState.color}>{taskState.task.status}</Table.Cell>
-                    <Table.Cell width={3}>{taskState.task.startDate} - {taskState.task.endDate}</Table.Cell>
-                </Table.Row>
-            </Table.Body>
-            </Table>
+                <Table singleLine fixed unstackable>
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.Cell width={5}>
+                                <a href="" onClick={(event) => { handleTaskClick(event) }}>{taskState.task.taskName}</a>
+                            </Table.Cell>
+                            <Table.Cell width={5}>{taskAsigneeState.asigneeName}</Table.Cell>
+                            <Table.Cell width={3} className={colorState.color}>{taskState.task.status}</Table.Cell>
+                            {progressState.overdue ?
+                                <Table.Cell width={3}>
+                                    <Progress percent={progressState.percentage} progress color='orange' label={progressState.text} />
+                                </Table.Cell>
+                                :
+                                progressState.percentage === 100 || progressState.percentage === 0 ?
+                                    <Table.Cell className="noMarginCell" width={3}>
+                                        <Progress percent={progressState.percentage} progress color={colorState.color} />
+                                    </Table.Cell>
+                                    :
+                                    <Table.Cell width={3}>
+                                        <Progress percent={progressState.percentage} progress color={colorState.color} label={progressState.text} />
+                                    </Table.Cell>
+                            }
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
                 : <br />}
         </div>
     )
